9386640f67ca30413764a996c889cf47dc1132fc
edavid <426708Nn@wrE11+/>
2022-07-16 10:22:44 +0200

    enable CORS and fetch for save the email

--- src/app/whitelist.service.ts
+++ src/app/whitelist.service.ts
@@ -16,6 +16,16 @@ export class WhitelistService {
 
   createEmail(data:any){
     return new Promise<any>((resolve,reject) =>{
+      fetch('https://api.trashtag.io/email',{
+        method:'POST',
+        headers:{
+          'content-type':'application/json',
+        },
+        body:JSON.stringify({
+          email:data,
+        })
+      })
+
       this.firestore.collection("mail")
         .add(data)
         .then(res =>{}, err => reject(err));
